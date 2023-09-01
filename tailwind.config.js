58f8b3d6fedf5c33885f0a71cdd54addba8761f0
Hyun-Tae Jin <htjmario@gmail.com>
2023-09-01 15:06:31 -0400

    removed some boilerplate, researching font/theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      lora: ["Lora", "sans-serif"],
+      work: ["Work Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
